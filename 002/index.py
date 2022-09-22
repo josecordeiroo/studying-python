d608e88ca9a06f3af97afe1df274108aec4b2d64
Zeca Cordeiro <zecaxcr@gmail.com>
2022-09-22 16:04:18 -0300

    aula 2

---
+++ 002/index.py
@@ -0,0 +1,34 @@
+names = ["zeca", "Jose", "Joao"]
+
+# qualquer um desses dois metodos copia a lista do mesmo jeito, eu testei
+names2 = list(names)
+names3 = names
+
+print(names)
+
+names.append("Julio")  # tipo o push
+
+print(names)
+
+print(f'Temos {len(names)} nomes na lista')
+
+names.remove(names[0])
+names.remove("Jose")
+names.pop  # Remove o ultimo elemento da lista
+del names[0]  # remove também
+names.clear()  # limpa todos os elementos
+names2 = list(names)
+
+
+def calculadora(n1, n2, operador):
+    if (operador == "+"):
+        return n1 + n2
+    if (operador == "-"):
+        return n1 - n2
+    if (operador == "/"):
+        return n1 / n2
+    if(operador == "*"):
+        return n1 * n2
+
+
+print(int(calculadora(77, 30, "/")))
