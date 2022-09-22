92e487577253068290d61c94c4f88a19e4ad98a6
Zeca Cordeiro <zecaxcr@gmail.com>
2022-09-22 16:34:44 -0300

    up

--- 003/index.py
+++ 003/index.py
@@ -1,4 +1,10 @@
-numbers=[1, 2, 3, 4, 5, 6]
+numbers=[1, 2, 7, 4, 5, 6]
 
 for i in numbers:
-    print(f'O numero é {i}')
+    print(f'O numero é {i}') 
+
+
+for i in numbers:
+    if (i == 7):
+        print("cheguei")
+        break
