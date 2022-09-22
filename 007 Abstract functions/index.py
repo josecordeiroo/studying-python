56769d1be0204341695e2ed5a7ee5099718430f7
Zeca Cordeiro <zecaxcr@gmail.com>
2022-09-22 20:24:15 -0300

    up

--- 007 Abstract functions/index.py
+++ 007 Abstract functions/index.py
@@ -3,6 +3,16 @@ def cores(*t):
     print(t[1])
     print(t[2])
 
+
 cores("azul", "branco", "vermelho")
 
-#função abstrata, não preciso especificar nada antes, mando do jeito que eu quiser
+# função abstrata, não preciso especificar nada antes, mando do jeito que eu quiser
+
+
+def cores(*t):
+
+    for i in t:
+        print(i)
+
+
+cores("azul", "branco", "vermelho")
