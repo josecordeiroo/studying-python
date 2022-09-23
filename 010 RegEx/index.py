8d8e51c1c9fa7b4843d19cdb49b3a29fde9fc525
Zeca Cordeiro <zecaxcr@gmail.com>
2022-09-23 15:50:51 -0300

    up

--- 010 RegEx/index.py
+++ 010 RegEx/index.py
@@ -1,10 +1,12 @@
 import re #RegEx
 
-texto="quando surge o alviverde imponente no gramado em que a luta o aguarda"
+texto="quando surge, o alviverde imponente no gramado, em que a luta o aguarda"
 
 res=re.findall("a", texto) #esse retorna todos os encontrados
 
 res=re.search("letra", texto) #esse somente indica se encontrou ou nao
 
 res=re.split("\s", texto) # cria um array com cada palavra entre os espaços
+
+res=re.sub("a","b", texto) # percorre toda a string trocando A por B
 
