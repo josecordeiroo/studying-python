76c58e1fdf90b0dc0200e90b9a578ab5d4ff5c29
Zeca Cordeiro <zecaxcr@gmail.com>
2022-09-23 15:45:17 -0300

    up

--- 010 RegEx/index.py
+++ 010 RegEx/index.py
@@ -2,6 +2,8 @@ import re #RegEx
 
 texto="quando surge o alviverde imponente no gramado em que a luta o aguarda"
 
-res=re.findall("a", texto)
+res=re.findall("a", texto) #esse retorna todos os encontrados
 
 print(f"Letra encontrada {len(res)} vezes.")
+
+res=re.search("letra", texto) #esse somente indica se encontrou ou nao
